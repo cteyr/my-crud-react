1ba2be3a9e6d360828acea19ab37b2c1c99d2d10
cteyr123 <cteyr123@gmail.com>
2022-07-05 18:08:52 +0200

    input todo add

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -1,4 +1,53 @@
+import { Button } from "@mui/material";
+import { Box } from "@mui/material";
+import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
+import AddBoxRoundedIcon from "@mui/icons-material/AddBoxRounded";
+import TextField from "@mui/material/TextField";
+
 const MainContainer = () => {
-  return <div className="div">Hola mundo</div>;
+  return (
+    <div className="main-container">
+      <h1>Todo App</h1>
+      <div className="container">
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            height: "3rem",
+            //backgroundColor: "blue",
+          }}
+        >
+          <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+            <FeaturedPlayListIcon
+              sx={{ color: "action.active", mr: 1, my: 0.5 }}
+            />
+            <TextField
+              id="input-with-sx"
+              label="Create a new Todo"
+              variant="standard"
+            />
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              marginTop: "0.6rem",
+              backgroundColor: "green",
+            }}
+          >
+            <Button
+              variant="contained"
+              startIcon={<AddBoxRoundedIcon />}
+              color="success"
+            >
+              Add
+            </Button>
+          </Box>
+        </Box>
+      </div>
+    </div>
+  );
 };
 export { MainContainer };
