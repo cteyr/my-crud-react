eb44c1d4a5ceb6451520d1e6cfaf62f87c58d6e8
cteyr123 <cteyr123@gmail.com>
2022-07-06 21:37:28 +0200

    function add to list finish

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -1,10 +1,33 @@
+import * as React from "react";
+import { useState } from "react";
 import { Button } from "@mui/material";
 import { Box } from "@mui/material";
 import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
 import AddBoxRoundedIcon from "@mui/icons-material/AddBoxRounded";
 import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import IconButton from "@mui/material/IconButton";
+import ListItemText from "@mui/material/ListItemText";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
 
 const MainContainer = () => {
+  const [Item, setItems] = useState([]);
+  const [InputValue, setInputValue] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const HandelClickAddtoList = () => {
+    if (InputValue != "") {
+      setItems((prev) => [InputValue, ...prev]);
+      setInputValue("");
+    }
+  };
+
   return (
     <div className="main-container">
       <h1>Todo App</h1>
@@ -16,17 +39,17 @@ const MainContainer = () => {
             alignItems: "center",
             width: "100%",
             height: "3rem",
-            //backgroundColor: "blue",
           }}
         >
           <Box sx={{ display: "flex", alignItems: "flex-end" }}>
-            <FeaturedPlayListIcon
-              sx={{ color: "action.active", mr: 1, my: 0.5 }}
-            />
+            <FeaturedPlayListIcon sx={{ color: "#2196f3", mr: 1, my: 0.5 }} />
             <TextField
               id="input-with-sx"
               label="Create a new Todo"
               variant="standard"
+              className="inputTodo"
+              value={InputValue}
+              onChange={handleChange}
             />
           </Box>
 
@@ -34,17 +57,38 @@ const MainContainer = () => {
             sx={{
               display: "flex",
               marginTop: "0.6rem",
-              backgroundColor: "green",
             }}
           >
             <Button
               variant="contained"
               startIcon={<AddBoxRoundedIcon />}
               color="success"
+              onClick={HandelClickAddtoList}
+              sx={{
+                borderRadius: "5 px",
+              }}
             >
               Add
             </Button>
           </Box>
+        </Box>
+
+        <Box className="list-container">
+          <Grid item xs={12} md={6} className="list-items">
+            <List>
+              {Item.map((element, index) => (
+                <ListItem className="items" key={index}>
+                  <ListItemText primary={element} />
+                  <IconButton aria-label="delete" color="primary">
+                    <ModeEditIcon />
+                  </IconButton>
+                  <IconButton aria-label="delete" className="remove-item">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItem>
+              ))}
+            </List>
+          </Grid>
         </Box>
       </div>
     </div>
