54d87ddb951677238dc171fbc3ee5cad9a0e4a76
cteyr123 <cteyr123@gmail.com>
2022-07-31 17:24:31 +0200

    useRedux implemented

--- src/containers/FooterContainer.tsx
+++ src/containers/FooterContainer.tsx
@@ -1,10 +1,62 @@
 import { Box } from "@mui/material";
+import { useReducer } from "react";
+
+const initialState = {
+  all: "selected",
+  active: "",
+  completed: "",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "all":
+      return {
+        all: "selected",
+        active: "",
+        completed: "",
+      };
+    case "active":
+      return {
+        all: "",
+        active: "selected",
+        completed: "",
+      };
+    case "selected":
+      return {
+        all: "",
+        active: "",
+        completed: "selected",
+      };
+  }
+  return state;
+};
+
 const FooterContainer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const HandelSelectAll = () => {
+    dispatch({ type: "all" });
+  };
+
+  const HandelSelectActive = () => {
+    dispatch({ type: "active" });
+  };
+
+  const HandelSelectSelected = () => {
+    dispatch({ type: "selected" });
+  };
+
   return (
     <Box className="footer-container">
-      <p>All</p>
-      <p>Active</p>
-      <p>Completed</p>
+      <p className={`${state.all}`} onClick={HandelSelectAll}>
+        <strong>All</strong>
+      </p>
+      <p className={`${state.active}`} onClick={HandelSelectActive}>
+        <strong>Active</strong>
+      </p>
+      <p className={`${state.completed}`} onClick={HandelSelectSelected}>
+        <strong>Completed</strong>
+      </p>
     </Box>
   );
 };
